b1452d1842d22e85f5449bd17765d64d7c998c11
singein <singein@outlook.com>
2020-01-17 15:08:03 +0800

    feat(wallpaper): 以异步的方式实现了爬取的业务

--- wallpapers/spider.py
+++ wallpapers/spider.py
@@ -1,29 +1,51 @@
 import requests
 from lxml import etree
 import asyncio
+import os
 import functools
-import future
+from concurrent.futures import Future
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(module)s %(message)s')
+logger = logging.getLogger('Papers')
+
+
 
 target_url = "http://simpledesktops.com/browse/"
 
-# html = requests.get(target_url).text
-# selector = etree.HTML(html)
-# imgs = selector.xpath('//div/a/img')
-# for img in imgs:
-#     print(img.attrib['src'])
+
+def get_page(page: int) -> str:
+    return target_url + str(page+1) + '/'
 
 
-def crawl_wallpapers(pages: int, processer: int, download_folder: str):
-    pass
+async def crawl_wallpapers(loop, page: int, download_folder: str = None):
+    response = await loop.run_in_executor(
+        None, functools.partial(requests.get, get_page(page)))
+    html = response.text
+    selector = etree.HTML(html)
+    imgs = selector.xpath('//div/a/img')
+
+    def download(url: str):
+        filename = f"{url[::-1].split('/', 1)[0][::-1]}"
+        with open(os.path.join(download_folder, filename), 'wb') as f:
+            response = requests.get(url)
+            for chunk in response.iter_content(512):
+                f.write(chunk)
+        logger.info(f"Downloading complete {img.attrib['src']}")
 
 
-async def get_html(url: str):
-    print('start get')
-    future = asyncio.get_event_loop().run_in_executor(
-        None, functools.partial(requests.get, url))
-    response = await future
-    print(response.text)
+    for img in imgs:
+        logger.info(f"Downloading {img.attrib['src']}")
+        await loop.run_in_executor(None, functools.partial(download,img.attrib['src']))
+
+
 
 if __name__ == "__main__":
+    download_path = os.path.join(os.path.dirname(
+        os.path.dirname(__file__)), 'download')
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait(get_html('http://simpledesktops.com/browse/')))
+    for i in range(100):
+        asyncio.ensure_future(
+            crawl_wallpapers(loop, i, download_path))
+    loop.run_forever()
